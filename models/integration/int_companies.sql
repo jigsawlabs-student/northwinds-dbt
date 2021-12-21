0f55c858a7e7d6a83feee607a90e851254cf24f8
jeff@jigsawlabs.io <jeff@jigsawlabs.io>
2021-12-21 19:10:45 +0000

    add int_companies with hubspot and rds data

---
+++ models/integration/int_companies.sql
@@ -0,0 +1,16 @@
+with hubspot_companies as (
+    select * from {{ ref('stg_hubspot_companies') }}
+), rds_companies as (
+    select * from {{ ref('stg_rds_companies') }}
+), merged_companies as (
+    select company_id as hubspot_company_id, null as rds_company_id, name from hubspot_companies union all
+    select null as hubspot_company_id, company_id as rds_company_id, name from rds_companies
+), deduped as (
+    select 
+    max(hubspot_company_id) as hubspot_company_id,
+    max(rds_company_id) as rds_company_id, name
+     from merged_companies group by name
+)
+ select hubspot_company_id, rds_company_id, deduped.name, address, city, postal_code, country from deduped
+  join rds_companies on rds_companies.company_id = deduped.rds_company_id
+
--- models/integration/int_contacts.sql
+++ models/integration/int_contacts.sql
@@ -4,17 +4,32 @@
      select * from {{ ref('stg_rds_customers') }} 
  ),
   merged_contacts as (
-    SELECT contact_id,
+    SELECT 
+    contact_id as hubspot_contact_id,
+    null as rds_contact_id,
     first_name, 
     last_name,
-    phone FROM contacts
+    phone, 
+    company_id as hubspot_company_id,
+    null as rds_company_id
+     FROM contacts
     union all 
-    SELECT customer_id as contact_id,
+    SELECT 
+    null as hubspot_contact_id,
+    customer_id as rds_contact_id,
     first_name, 
     last_name,
-    phone FROM customers
+    phone, 
+    null as hubspot_company_id,
+    company_id as rds_company_id
+    FROM customers
  ), final as (
-     select array_agg(contact_id) as contact_ids, first_name, last_name, max(phone) as phone
-     from merged_contacts group by first_name, last_name ORDER BY last_name
+     select max(hubspot_contact_id) as hubspot_contact_id, max(rds_contact_id) as rds_contact_id,
+        first_name, last_name, max(phone) as phone, 
+        max(hubspot_company_id) as hubspot_company_id, max(rds_company_id) rds_company_id
+     from merged_contacts
+      group by first_name, last_name ORDER BY last_name
  )
- select * from final
+ select first_name, last_name, name from final
+  join {{ ref('int_companies') }} int_companies on 
+  int_companies.rds_company_id = final.rds_company_id or int_companies.hubspot_company_id = final.hubspot_company_id
