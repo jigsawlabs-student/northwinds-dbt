44513d7f6f032315842085727ad5a1050e9f3f8f
jeff@jigsawlabs.io <jeff@jigsawlabs.io>
2021-12-25 22:20:30 +0000

    refactor contacts

--- models/integration/int_contacts.sql
+++ models/integration/int_contacts.sql
@@ -1,28 +1,19 @@
- with contacts as (
-     select * from {{ ref('stg_hubspot_contacts') }}
- ), customers as (
-     select * from {{ ref('stg_rds_customers') }} 
- ),
+{% set sources = ['stg_hubspot_contacts', 'stg_rds_customers'] %}
+
+ with
   merged_contacts as (
+    {% for source in sources %}
     SELECT 
-    contact_id as hubspot_contact_id,
-    null as rds_contact_id,
+    {{ 'contact_id' if 'hubspot' in source else 'null' }} as hubspot_contact_id,
+      {{ 'customer_id' if 'rds' in source else 'null' }} as rds_contact_id,
     first_name, 
     last_name,
-    phone, 
-    company_id as hubspot_company_id,
-    null as rds_company_id
-     FROM contacts
-    union all 
-    SELECT 
-    null as hubspot_contact_id,
-    customer_id as rds_contact_id,
-    first_name, 
-    last_name,
-    phone, 
-    null as hubspot_company_id,
-    company_id as rds_company_id
-    FROM customers
+    phone,
+     {{ 'company_id' if 'hubspot' in source else 'null' }} as hubspot_company_id,
+        {{ 'company_id' if 'rds' in source else 'null' }} as rds_company_id
+     FROM {{ ref(source)}}
+    {% if not loop.last %} union all {% endif %}
+    {% endfor %}
  ), final as (
      select 
      max(hubspot_contact_id) as hubspot_contact_id, max(rds_contact_id) as rds_contact_id,
@@ -31,5 +22,6 @@
      from merged_contacts
       group by first_name, last_name
  )
- select {{ dbt_utils.surrogate_key(['first_name', 'last_name', 'phone']) }} as contact_pk, hubspot_contact_id, rds_contact_id,
+ select {{ dbt_utils.surrogate_key(['first_name', 'last_name', 'phone']) }} as contact_pk,
+  hubspot_contact_id, rds_contact_id,
   first_name, last_name, phone, hubspot_company_id, rds_company_id from final 
